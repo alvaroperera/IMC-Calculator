9ad227ec1834c7f5dbcfb56684caad24623832df
Álvaro Perera <alvprodz@gmail.com>
2024-12-04 20:14:33 +0100

    Initial Commit

---
+++ IMC CalculatorTests/IMC_CalculatorTests.swift
@@ -0,0 +1,17 @@
+//
+//  IMC_CalculatorTests.swift
+//  IMC CalculatorTests
+//
+//  Created by Álvaro Perera on 4/12/24.
+//
+
+import Testing
+@testable import IMC_Calculator
+
+struct IMC_CalculatorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
