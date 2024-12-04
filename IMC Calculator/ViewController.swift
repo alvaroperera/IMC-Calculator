5faa605af21a7907cd06347d1211df45e2abde84
Álvaro Perera <alvprodz@gmail.com>
2024-12-04 23:17:18 +0100

    20241204_APR_ProjectFilesAddedAppWorking
    - Simple form created
    - IMC generated and text explaining results
    - Text color changes depending on results

--- IMC Calculator/ViewController.swift
+++ IMC Calculator/ViewController.swift
@@ -9,6 +9,38 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var heightText: UITextField!
+    @IBOutlet weak var weightText: UITextField!
+    @IBOutlet weak var calculateButton: UIButton!
+    @IBOutlet weak var labelStringResult: UILabel!
+    @IBOutlet weak var resultText: UILabel!
+    @IBAction func calculateIMC(_ sender: UIButton) {
+        let height = Float(heightText.text!) ?? 0
+        let weight = Float(weightText.text!) ?? 0
+        let imc = weight / (height/100 * height/100)
+        
+        if imc >= 30.00 {
+            resultText.textColor = .red
+            resultText.text = "\(imc)"
+            labelStringResult.textColor = .red
+            labelStringResult.text = "Obeso"
+        } else if imc >= 25.00 {
+            resultText.textColor = .yellow
+            resultText.text = "\(imc)"
+            labelStringResult.textColor = .yellow
+            labelStringResult.text = "Sobrepeso"
+        } else if imc >= 18.49 {
+            resultText.textColor = .green
+            resultText.text = "\(imc)"
+            labelStringResult.textColor = .green
+            labelStringResult.text = "Normal"
+        } else {
+            resultText.textColor = .blue
+            resultText.text = "\(imc)"
+            labelStringResult.textColor = .blue
+            labelStringResult.text = "Bajo peso"
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
